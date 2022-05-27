21572046d7dc36cf4a7e92f91719cd88eed716f5
srinu391 <srinu@comdex.sg>
2022-05-27 09:56:18 +0530

    Added amino converters  for vault messages.

--- src/services/aminoConverter.js
+++ src/services/aminoConverter.js
@@ -51,4 +51,165 @@ export const customAminoTypes = {
       };
     },
   },
+  "/comdex.vault.v1beta1.MsgDepositRequest": {
+    aminoType: "vault/MsgDepositRequest",
+    toAmino: ({
+      from,
+      appMappingId,
+      extendedPairVaultId,
+      userVaultId,
+      amount,
+    }) => {
+      return {
+        from,
+        app_mapping_id: String(appMappingId),
+        extended_pair_vault_id: String(extendedPairVaultId),
+        user_vault_id: userVaultId,
+        amount: amount,
+      };
+    },
+    fromAmino: ({
+      from,
+      app_mapping_id,
+      extended_pair_vault_id,
+      user_vault_id,
+      amount,
+    }) => {
+      return {
+        from,
+        appMappingId: Number(app_mapping_id),
+        extendedPairVaultId: Number(extended_pair_vault_id),
+        userVaultId: user_vault_id,
+        amount: amount,
+      };
+    },
+  },
+  "/comdex.vault.v1beta1.MsgWithdrawRequest": {
+    aminoType: "vault/MsgWithdrawRequest",
+    toAmino: ({
+      from,
+      appMappingId,
+      extendedPairVaultId,
+      userVaultId,
+      amount,
+    }) => {
+      return {
+        from,
+        app_mapping_id: String(appMappingId),
+        extended_pair_vault_id: String(extendedPairVaultId),
+        user_vault_id: userVaultId,
+        amount: amount,
+      };
+    },
+    fromAmino: ({
+      from,
+      app_mapping_id,
+      extended_pair_vault_id,
+      user_vault_id,
+      amount,
+    }) => {
+      return {
+        from,
+        appMappingId: Number(app_mapping_id),
+        extendedPairVaultId: Number(extended_pair_vault_id),
+        userVaultId: user_vault_id,
+        amount: amount,
+      };
+    },
+  },
+  "/comdex.vault.v1beta1.MsgDrawRequest": {
+    aminoType: "vault/MsgDrawRequest",
+    toAmino: ({
+      from,
+      appMappingId,
+      extendedPairVaultId,
+      userVaultId,
+      amount,
+    }) => {
+      return {
+        from,
+        app_mapping_id: String(appMappingId),
+        extended_pair_vault_id: String(extendedPairVaultId),
+        user_vault_id: userVaultId,
+        amount: amount,
+      };
+    },
+    fromAmino: ({
+      from,
+      app_mapping_id,
+      extended_pair_vault_id,
+      user_vault_id,
+      amount,
+    }) => {
+      return {
+        from,
+        appMappingId: Number(app_mapping_id),
+        extendedPairVaultId: Number(extended_pair_vault_id),
+        userVaultId: user_vault_id,
+        amount: amount,
+      };
+    },
+  },
+  "/comdex.vault.v1beta1.MsgRepayRequest": {
+    aminoType: "vault/MsgRepayRequest",
+    toAmino: ({
+      from,
+      appMappingId,
+      extendedPairVaultId,
+      userVaultId,
+      amount,
+    }) => {
+      return {
+        from,
+        app_mapping_id: String(appMappingId),
+        extended_pair_vault_id: String(extendedPairVaultId),
+        user_vault_id: userVaultId,
+        amount: amount,
+      };
+    },
+    fromAmino: ({
+      from,
+      app_mapping_id,
+      extended_pair_vault_id,
+      user_vault_id,
+      amount,
+    }) => {
+      return {
+        from,
+        appMappingId: Number(app_mapping_id),
+        extendedPairVaultId: Number(extended_pair_vault_id),
+        userVaultId: user_vault_id,
+        amount: amount,
+      };
+    },
+  },
+  "/comdex.vault.v1beta1.MsgCloseRequest": {
+    aminoType: "vault/MsgCloseRequest",
+    toAmino: ({
+      from,
+      appMappingId,
+      extendedPairVaultId,
+      userVaultId,
+    }) => {
+      return {
+        from,
+        app_mapping_id: String(appMappingId),
+        extended_pair_vault_id: String(extendedPairVaultId),
+        user_vault_id: userVaultId,
+      };
+    },
+    fromAmino: ({
+      from,
+      app_mapping_id,
+      extended_pair_vault_id,
+      user_vault_id,
+    }) => {
+      return {
+        from,
+        appMappingId: Number(app_mapping_id),
+        extendedPairVaultId: Number(extended_pair_vault_id),
+        userVaultId: user_vault_id,
+      };
+    },
+  },
 };
