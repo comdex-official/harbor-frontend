a7cf3ea57c6a777ac1b847e3fcd259d69136c367
srinu391 <srinu@comdex.sg>
2022-05-27 19:20:50 +0530

    Update config file.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import SvgSprite from "./utils/SvgSpriteLoader";
 import svgFile from "./assets/images/svg/svg-sprite.svg";
 import SideBar from "./containers/SideBar";
 import { useEffect } from "react";
+import {message} from 'antd';
+
+message.config({
+  maxCount: 2,
+});
 
 function App() {
 
