66088798c1ab4816ab2322898a79438bd5cfafa4
srinu391 <srinu@comdex.sg>
2022-05-30 17:18:56 +0530

    Added auction transactions in registry.

--- src/services/registry.js
+++ src/services/registry.js
@@ -1,6 +1,8 @@
 import { MsgCreateLockerRequest, MsgDepositAssetRequest, MsgWithdrawAssetRequest } from 'comdex-codec/build/comdex/locker/v1beta1/msg';
 import { MsgCreateRequest, MsgDepositRequest, MsgWithdrawRequest, MsgDrawRequest, MsgRepayRequest, MsgCloseRequest } from 'comdex-codec/build/comdex/vault/v1beta1/msg'
 
+import {MsgPlaceSurplusBidRequest, MsgPlaceDebtBidRequest, MsgPlaceDutchBidRequest} from 'comdex-codec/build/comdex/auction/v1beta1/msg';
+
 import { Registry } from "@cosmjs/proto-signing";
 import { defaultRegistryTypes } from "@cosmjs/stargate";
 
@@ -15,6 +17,10 @@ export const myRegistry = new Registry([
   ["/comdex.vault.v1beta1.MsgDrawRequest", MsgDrawRequest],
   ["/comdex.vault.v1beta1.MsgRepayRequest", MsgRepayRequest],
   ["/comdex.vault.v1beta1.MsgCloseRequest", MsgCloseRequest],
+  ["/comdex.auction.v1beta1.MsgPlaceSurplusBidRequest", MsgPlaceSurplusBidRequest],
+  ["/comdex.auction.v1beta1.MsgPlaceDebtBidRequest", MsgPlaceDebtBidRequest],
+  ["/comdex.auction.v1beta1.MsgPlaceDutchBidRequest", MsgPlaceDutchBidRequest],
+  
 ]);
 
 
