a41a40d69425eb527d43d84dad5b6f545898fe3d
Priyanshu1710 <guptapriyanshu1710@gmail.com>
2022-08-23 21:46:49 +0530

    Added harbor poolId in .env file.

--- src/constants/common.js
+++ src/constants/common.js
@@ -4,4 +4,4 @@ export const DEFAULT_FEE = 100000;
 export const DOLLAR_DECIMALS = 2;
 export const PRODUCT_ID = Number(process.env.REACT_APP_HARBOR_APP_ID) ;
 export const CSWAP_APP_ID = Number(process.env.REACT_APP_CSWAP_APP_ID); // for testnet appId is 1, and for devnet appId is 2
-export const HARBOR_POOL_ID_LIST = [1];
+export const HARBOR_POOL_ID_LIST = process.env.REACT_APP_HARBOR_POOL_ID_LIST;
