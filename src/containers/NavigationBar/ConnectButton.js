b1d15da374f90726f0decb2d2cf25ed6cb859e14
Priyanshu1710 <guptapriyanshu1710@gmail.com>
2022-12-27 13:53:28 +0530

    Updated condition for decode address.

--- src/containers/NavigationBar/ConnectButton.js
+++ src/containers/NavigationBar/ConnectButton.js
@@ -121,7 +121,7 @@ const ConnectButton = ({
 
   useEffect(() => {
     let addressAlreadyExist = localStorage.getItem("ac");
-    addressAlreadyExist = decode(addressAlreadyExist)
+    addressAlreadyExist = addressAlreadyExist ? decode(addressAlreadyExist) : "";
     setAddressFromLocal(addressAlreadyExist)
   }, [])
 
