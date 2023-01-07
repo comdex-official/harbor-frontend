d299f4fa8e607b5714c157c90bd35c98baaf47a9
asif <asif.ansari7774@gmail.com>
2023-01-07 18:56:29 +0530

    removed christmas theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Router from "./Router";
-import Snowfall from 'react-snowfall';
 import NavigationBar from "./containers/NavigationBar";
 import SvgSprite from "./utils/SvgSpriteLoader";
 import svgFile from "./assets/images/svg/svg-sprite.svg";
@@ -17,15 +16,6 @@ const App = () => {
     <>
       <SvgSprite url={svgFile} />
       <div className="main_wrapper">
-        <Snowfall
-          snowflakeCount={70}
-          style={{
-            position: 'fixed',
-            width: '100vw',
-            height: '100vh',
-            zIndex: '999',
-          }}
-        />
         <SideBar />
         <div className="main-container">
           <NavigationBar />
