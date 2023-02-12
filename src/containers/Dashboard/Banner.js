a6c00f223734487a89edefefc9ef3d28924cfd3f
Priyanshu1710 <guptapriyanshu1710@gmail.com>
2023-02-12 19:00:11 +0530

    Updated dashboard banner.

--- src/containers/Dashboard/Banner.js
+++ src/containers/Dashboard/Banner.js
@@ -4,18 +4,12 @@ import { Link } from "react-router-dom";
 
 const Banner = ({ lang }) => {
   return (
-    <div className="dashboard-banner earn-deposite-card">
-      <div className="banner-left">
-        <h2>
-          Borrow <span>Composite</span>  by depositing <br /> your IBC assets
-        </h2>
-        <Link to='/mint'>
-          <Button type="primary " className=" btn-filled ">
-            Take me there!
-          </Button>
-        </Link>
+    <Link to="/more/airdrop">
+      <div className="dashboard-banner earn-deposite-card">
+        <div className="banner-left">
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
