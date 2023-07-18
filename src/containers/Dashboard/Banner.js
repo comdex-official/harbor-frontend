ab9f02bb8f95fe7e15dd6025c8de4e1e8ce419f9
Priyanshu1710 <guptapriyanshu1710@gmail.com>
2023-07-18 20:01:58 +0530

    Updated dashboard banner.

--- src/containers/Dashboard/Banner.js
+++ src/containers/Dashboard/Banner.js
@@ -6,14 +6,27 @@ const Banner = ({ lang }) => {
   return (
     <div className="dashboard-banner earn-deposite-card">
       <div className="banner-left">
-        <h2>
+        {/* <h2>
           Borrow <span>Composite</span>  by depositing <br /> your IBC assets
         </h2>
         <Link to='/mint'>
           <Button type="primary " className=" btn-filled ">
             Take me there!
           </Button>
+        </Link> */}
+        <h2>
+          Weekly Emissions are Live!
+        </h2>
+        <p>
+          Lock your HARBOR, Vote on Emissions and Claim your Rewards.
+        </p>
+        <div className="btn_container">
+        <Link to='/more/vote'>
+          <Button type="primary " className=" btn-filled ">
+            Vote Now
+          </Button>
         </Link>
+        </div>
       </div>
     </div>
   );
